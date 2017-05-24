8c165f7c01cda6b9b59eb64bf3c77e311681f51d
Daniel Saewitz <switz213@gmail.com>
2017-05-24 04:23:05 -0400

    Animate queue on track change

--- components/Queue.js
+++ components/Queue.js
@@ -8,6 +8,15 @@ import player from '../lib/player'
 import Row from './Row'
 
 export default class Queue extends Component {
+  componentWillReceiveProps(nextProps) {
+    const { playback } = this.props;
+
+    if (playback.activeTrack.idx !== nextProps.playback.activeTrack.idx && this.queue) {
+      const nextTrack = this.queue.querySelector(`[data-idx="${nextProps.playback.activeTrack.idx}"`)
+      if (nextTrack) this.scrollToPos(this.queue, nextTrack.offsetTop - this.queue.offsetTop - 16, 600)
+    }
+  }
+
   render() {
     const { playback, tapes } = this.props;
 
@@ -15,7 +24,7 @@ export default class Queue extends Component {
     const tape = tapes[playback.artistSlug] ? tapes[playback.artistSlug][playback.showDate] : null
 
     return (
-      <div className="queue">
+      <div className="queue" ref={ref => this.queue = ref}>
         <style jsx>{`
           .queue {
             width: 33%;
@@ -24,6 +33,7 @@ export default class Queue extends Component {
             flex-direction: column;
             overflow-y: scroll;
             cursor: pointer;
+            transition: scroll
           }
 
           .queue .active {
@@ -42,6 +52,7 @@ export default class Queue extends Component {
             <Row
               onClick={() => player.gotoTrack(idx, true)}
               active={idx === playback.activeTrack.idx}
+              data-idx={idx}
               key={idx}>
               <div>{track.title}</div>
               <div onClick={this.goToShow}>
@@ -53,6 +64,35 @@ export default class Queue extends Component {
         })}
       </div>
     )
+  }
+
+  scrollToPos = (element, to, duration) => {
+    var start = element.scrollTop,
+        change = to - start,
+        currentTime = 0,
+        increment = 16;
+
+    //t = current time
+    //b = start value
+    //c = change in value
+    //d = duration
+    const easeInOutQuad = function (t, b, c, d) {
+      t /= d/2;
+      if (t < 1) return c/2*t*t + b;
+      t--;
+      return -c/2 * (t*(t-2) - 1) + b;
+    };
+
+    var animateScroll = function(){
+        currentTime += increment;
+        element.scrollTop = easeInOutQuad(currentTime, start, change, duration);
+
+        if (currentTime < duration) {
+            window.requestAnimationFrame(animateScroll);
+        }
+    };
+
+    window.requestAnimationFrame(animateScroll);
   }
 
   goToShow = (e) => {
