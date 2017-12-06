d3f761748bb8a50251ba57321869c634a3bcb5e9
Daniel Saewitz <switz213@gmail.com>
2017-12-06 12:36:02 -0500

    Basic about page

--- pages/about.js
+++ pages/about.js
@@ -11,9 +11,43 @@ const About = () => (
       .page-container {
         flex: 1;
       }
+
+      .italic {
+        font-style: italic;
+      }
     `}</style>
     <div className="page-container">
-      about
+      <p>
+        Welcome to Relisten.
+      </p>
+
+      <p>
+        Peruse the various bands and enjoy their extensive live catalogues for free.
+      </p>
+
+      <p>
+        This site is powered by Archive.org, Phish.in, and PanicStream.
+      </p>
+
+      <p>
+        Everything on our end is open source at <a href="https://github.com/relistennet" target="_blank">https://github.com/relistennet</a>
+      </p>
+
+      <p>
+        Enjoy <br />
+        - Daniel Saewitz and Alec Gorge, admin@relisten.net
+      </p>
+
+      <br />
+      <br />
+
+      <p>
+        This site complies with Archive.org's <a href="https://archive.org/post/261115/hotlinking-allowed" target="_blank">policy</a>.
+      </p>
+
+      <p className="italic">
+        The Grateful Dead and our managing organizations have long encouraged the purely non-commercial exchange of music taped at our concerts and those of our individual members. That a new medium of distribution has arisen - digital audio files being traded over the Internet - does not change our policy in this regard. Our stipulations regarding digital distribution are merely extensions of those long-standing principles and they are as follow: No commercial gain may be sought by websites offering digital files of our music, whether through advertising, exploiting databases compiled from their traffic, or any other means. All participants in such digital exchange acknowledge and respect the copyrights of the performers, writers and publishers of the music. This notice should be clearly posted on all sites engaged in this activity. We reserve the ability to withdraw our sanction of non-commercial digital music should circumstances arise that compromise our ability to protect and steward the integrity of our work.
+      </p>
     </div>
   </Layout>
 )
