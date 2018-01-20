26ad642ebf084bc5fc5dad26fae9bba6b8174820
Daniel Saewitz <switz213@gmail.com>
2018-01-20 16:51:32 -0500

    Update live.js

--- pages/live.js
+++ pages/live.js
@@ -35,9 +35,9 @@ class Live extends Component {
           Live
 
           {live.data.map(data =>
-            <div key={data.track.id}>
+            data.track && data.source ? <div key={data.track.id}>
               {data.track.title} - {data.source.artist.name}
-            </div>
+            </div> : null
           )}
         </div>
         <style jsx>{`
