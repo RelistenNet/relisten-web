a6bad9929ac63fe032475a20c1ac6387acb38e3e
Daniel Saewitz <switz213@gmail.com>
2018-09-05 04:40:38 -0400

    Add Sentry support

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,19 @@ import App, { Container } from 'next/app';
 import React from 'react';
 import { Provider } from 'react-redux';
 import withRedux from 'next-redux-wrapper'
+import Raven from 'raven-js'
 
 import { initStore } from '../redux'
 
+const SENTRY_PUBLIC_DSN = 'https://d8fe64a30ead43e2ac70c750bc79a806@sentry.io/1261843'
+
 class MyApp extends App {
+  constructor(...args) {
+    super(...args);
+
+    Raven.config(SENTRY_PUBLIC_DSN).install();
+  }
+
   static async getInitialProps({ Component, router, ctx }) {
     let pageProps = {};
 
@@ -14,6 +23,13 @@ class MyApp extends App {
     }
 
     return { pageProps };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    Raven.captureException(error, { extra: errorInfo })
+
+    // This is needed to render errors correctly in development / production
+    super.componentDidCatch(error, errorInfo)
   }
 
   render() {
