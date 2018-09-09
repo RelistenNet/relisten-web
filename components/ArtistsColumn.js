de3a4c02bbdc92bb4e62cde338e3521cdd501c5c
Daniel Saewitz <switz213@gmail.com>
2018-09-09 01:31:50 -0400

    Add byline to WSP & Phish

--- components/ArtistsColumn.js
+++ components/ArtistsColumn.js
@@ -6,6 +6,11 @@ import Column from './Column'
 import Row from './Row'
 import RowHeader from './RowHeader'
 
+const byObject = {
+  wsp: 'PanicStream',
+  phish: 'Phish.in'
+}
+
 const ArtistsColumn = ({ artists = {}, artistSlug }) => (
   <Column heading="Bands">
     {artists && artists.data && Object.entries(groupBy(Object.values(artists.data), 'featured')).sort(([a], [b]) => b - a).map(([type, artists]) =>
@@ -13,7 +18,10 @@ const ArtistsColumn = ({ artists = {}, artistSlug }) => (
         <RowHeader key={`header-${type}`}>{type === '1' ? 'Featured' : 'Bands'}</RowHeader>,
         ...artists.map((artist, idx) =>
           <Row key={[idx, artist.id].join(':')} href={`/${artist.slug}`} active={artist.slug === artistSlug}>
-            <div>{artist.name}</div>
+            <div>
+              {artist.name}
+              {byObject[artist.slug] && <span className="by">Powered by {byObject[artist.slug]}</span>}
+            </div>
             <div>
               <div>{simplePluralize('show', artist.show_count)}</div>
               <div>{simplePluralize('tape', artist.source_count)}</div>
@@ -22,6 +30,10 @@ const ArtistsColumn = ({ artists = {}, artistSlug }) => (
         )
       ]
     )}
+    <style jsx>{`
+      .by
+        font-size 8px
+    `}</style>
   </Column>
 )
 
