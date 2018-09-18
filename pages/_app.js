76ab085c95f2dbabd4f36a0e2a93458774a370b5
Daniel Saewitz <switz213@gmail.com>
2018-09-18 00:25:01 -0400

    Add array/fill

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,8 @@ import 'core-js/fn/string/starts-with'
 import 'core-js/fn/string/includes'
 import 'core-js/fn/array/includes'
 import 'core-js/fn/array/find'
-import 'core-js/es6/promise';
+import 'core-js/fn/array/fill'
+import 'core-js/es6/promise'
 
 import { initStore } from '../redux'
 
