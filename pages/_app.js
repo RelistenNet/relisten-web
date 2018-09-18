0e8bc6039b29ac4b63ed302800558ddc30d6efd4
Daniel Saewitz <switz213@gmail.com>
2018-09-18 00:20:36 -0400

    Add promise polyfill

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,7 @@ import 'core-js/fn/string/starts-with'
 import 'core-js/fn/string/includes'
 import 'core-js/fn/array/includes'
 import 'core-js/fn/array/find'
+import 'core-js/features/promise';
 
 import { initStore } from '../redux'
 
