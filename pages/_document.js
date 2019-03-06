282c2b3b02b3905210d7284caafd7153fc0d742e
Daniel Saewitz <switz213@gmail.com>
2019-03-06 12:40:03 +0000

    Downgrade react-transition-group to fix /live

--- pages/_document.js
+++ pages/_document.js
@@ -3,13 +3,13 @@ import Document, { Head, Main, NextScript } from 'next/document'
 import flush from 'styled-jsx/server'
 
 export default class MyDocument extends Document {
-  static getInitialProps ({ renderPage }) {
-    const { html, head, errorHtml, chunks } = renderPage()
-    const styles = flush()
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
 
-    return { html, head, errorHtml, chunks, styles }
+    return { ...initialProps }
   }
-  render () {
+
+  render() {
     const { fullPath } = this.props;
 
     return (
