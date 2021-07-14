18e86972d7e498e7244793637e5f787600f4045c
Alec Gorge <alecgorge@gmail.com>
2021-07-14 11:13:50 -0700

    Remove comment from next.config.js

--- next.config.js
+++ next.config.js
@@ -2,17 +2,4 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // webpack: function (config) {
-  //   config.module.rules.push({
-  //     test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-  //     use: {
-  //       loader: 'url-loader',
-  //       options: {
-  //         limit: 100000,
-  //         name: '[name].[ext]',
-  //       },
-  //     },
-  //   });
-  //   return config;
-  // },
 };
