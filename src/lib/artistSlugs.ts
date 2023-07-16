78c0a341409841105e49a95d6e421ebed26bd4cd
Thenlie <leithen113@gmail.com>
2023-07-16 16:32:36 -0600

    lib: migrate artistSlugs to typescript

--- src/lib/artistSlugs.ts
+++ src/lib/artistSlugs.ts
@@ -1,11 +1,12 @@
-const { API_DOMAIN } = require('./constants');
-const artistSlugs = require('./rawSlugs');
+import { Artist } from '../types';
+import { API_DOMAIN } from './constants';
+import artistSlugs from './rawSlugs';
 
 if (typeof window === 'undefined') {
   fetch(`${API_DOMAIN}/api/v2/artists`)
     .then((res) => res.json())
     .then((json) =>
-      json.map((artist) => {
+      json.map((artist: Artist) => {
         if (artistSlugs.indexOf(artist.slug) === -1) {
           artistSlugs.push(artist.slug);
         }
@@ -13,4 +14,4 @@ if (typeof window === 'undefined') {
     );
 }
 
-module.exports = artistSlugs;
+export { artistSlugs };
