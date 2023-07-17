308b8efa2bce3501d746641986d173f07ab2389c
Thenlie <leithen113@gmail.com>
2023-07-16 21:37:47 -0600

    lib: migrate sortActiveBands to typescript

--- src/lib/sortActiveBands.ts
+++ src/lib/sortActiveBands.ts
@@ -1,4 +1,4 @@
-export default (slug, arr = []) => {
+export default (slug: string, arr = []): unknown[] => {
   // TODO: check if bands have shows in the last 2 years to sort
   if (slug === 'phish') return [...arr].reverse();
 
