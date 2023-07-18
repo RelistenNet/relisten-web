6a9013bdc376c4f2921802e307660b96785ab6a5
Thenlie <leithen113@gmail.com>
2023-07-17 22:51:48 -0600

    pages: migrate _document to typescript

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,28 +1,24 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
 import Script from 'next/script';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
 
     return { ...initialProps };
   }
 
   render() {
+    const headStyle = `
+      body { margin: 0; font-family: Roboto, Helvetica, Helvetica Neue, sans-serif; -webkit-font-smoothing: antialiased; color: #333; }
+      a { text-decoration: none; color: #333; }
+    `
     return (
       <Html>
         <Head>
           <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
-          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-          <meta httpEquiv="Content-Language" content="en" />
-          <meta name="google" content="notranslate" />
           <link rel="icon" href="/favicon.ico" />
-          <style>
-            {`
-              body { margin: 0; font-family: Roboto, Helvetica, Helvetica Neue, sans-serif; -webkit-font-smoothing: antialiased; color: #333; }
-              a { text-decoration: none; color: #333; }
-            `}
-          </style>
+          <style>{headStyle}</style>
         </Head>
         <body>
           <Main />
