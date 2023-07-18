bd567cd4877ddeb4dbb24edb146442837f9a6ebb
Thenlie <leithen113@gmail.com>
2023-07-17 22:55:48 -0600

    layouts: migrate index to typescriptgst

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
 
-const Index = ({ children, ...props }) => (
+const Index = ({ children, ...props }): JSX.Element => (
   <div className="layout">
     <style jsx>{`
       .layout {
