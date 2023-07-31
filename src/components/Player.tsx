ec320277b8fa6dc157bd9749370725a624b12e1f
Thenlie <leithen113@gmail.com>
2023-07-30 18:35:03 -0600

    tw: migrate Player to tailwind

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link';
 import { durationToHHMMSS, removeLeadingZero, splitShowDate } from '../lib/utils';
 import player from '../lib/player';
 import { Artist, Meta, Playback } from '../types';
+import Flex from './Flex';
 
 type PlayerProps = {
   playback: Playback;
@@ -57,144 +58,7 @@ class Player extends Component<PlayerProps, PlayerState> {
           (this.player.clientWidth - 3);
 
     return (
-      <div className="container">
-        <style jsx>{`
-          .container {
-            height: 50px;
-            display: flex;
-            position: relative;
-          }
-
-          .container .fas {
-            cursor: pointer;
-          }
-
-          .playpause,
-          .queue-button {
-            width: 50px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            color: #7a7a7a;
-            cursor: pointer;
-          }
-
-          .playpause:active,
-          .queue-button:active {
-            color: #4a4a4a;
-          }
-
-          .player {
-            height: 100%;
-            flex: 1;
-            position: relative;
-          }
-
-          .content {
-            display: flex;
-            flex-direction: row;
-            justify-content: center;
-            transition: all 1s ease-in-out;
-            height: 100%;
-          }
-
-          .progress-container {
-            width: 100%;
-            height: 4px;
-            background: #bcbcbc;
-            position: absolute;
-            left: 0;
-            bottom: 0;
-            z-index: 1;
-            cursor: pointer;
-          }
-
-          .progress-background {
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            height: 4px;
-            background: #707070;
-            // transition: width 64ms linear;
-          }
-
-          .progress-notch {
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            width: 3px;
-            height: 8px;
-            background: #000;
-            // transition: transform 64ms linear;
-            z-index: 1;
-          }
-
-          .info {
-            display: flex;
-            flex-direction: column;
-            justify-content: space-around;
-            padding: 8px 0;
-          }
-
-          .song-title {
-            color: #3c3c3c;
-            font-size: 1em;
-            position: relative;
-          }
-
-          .after-song-title {
-            display: flex;
-            align-items: center;
-            position: absolute;
-            width: 100%;
-            left: 100%;
-            top: 2px;
-            margin-left: 8px;
-            font-size: 0.8em;
-            color: #7a7a7a;
-          }
-
-          .band-title {
-            color: #7a7a7a;
-            font-size: 0.8em;
-            justify-content: center;
-          }
-
-          .timing {
-            color: #7a7a7a;
-            font-size: 0.8em;
-            position: absolute;
-            left: 8px;
-            top: 50%;
-            transform: translate(0, -50%);
-            text-align: left;
-          }
-
-          .duration {
-            left: initial;
-            right: 8px;
-            text-align: right;
-          }
-
-          .volume-container {
-            height: 100%;
-            width: 6px;
-            background: rgba(0, 0, 0, 0.1);
-            position: absolute;
-            right: -6px;
-            top: 0;
-          }
-
-          .volume-bar {
-            background: #707070;
-            position: absolute;
-            top: 0;
-            bottom: 0;
-            left: 0;
-            right: 0;
-            pointer-events: none;
-          }
-        `}</style>
+      <Flex className="relative h-[50px]">
         {activeTrack && (
           <Head>
             <title>
@@ -208,27 +72,35 @@ class Player extends Component<PlayerProps, PlayerState> {
           </Head>
         )}
         {activeTrack && (
-          <div className="playpause" onClick={() => player.togglePlayPause()}>
-            <i className={`fas fa-${playback.activeTrack.isPaused ? 'play' : 'pause'}`} />
-          </div>
+          <Flex
+            className="w-[50px] cursor-pointer items-center justify-center text-[#7a7a7a] active:text-[#4a4a4a]"
+            onClick={() => player.togglePlayPause()}
+          >
+            <i
+              className={`fa cursor-pointer fa-${playback.activeTrack.isPaused ? 'play' : 'pause'}`}
+            />
+          </Flex>
         )}
         {typeof window === 'undefined' || !activeTrack ? null : (
-          <div className="player" ref={(ref) => (this.player = ref)}>
-            <div className="content">
-              <div className="timing">
+          <div className="relative h-full flex-1" ref={(ref) => (this.player = ref)}>
+            <Flex className="h-full justify-center transition-all duration-[1s] ease-in-out">
+              <div className="absolute left-[8px] top-1/2 translate-x-0 translate-y-[-50%] text-left text-[0.8em] text-[#7a7a7a]">
                 <div>
-                  <i className="fas fa-backward" onClick={() => player.playPrevious()} />
+                  <i
+                    className="fa fa-backward cursor-pointer"
+                    onClick={() => player.playPrevious()}
+                  />
                 </div>
                 <div>{durationToHHMMSS(playback.activeTrack.currentTime)}</div>
               </div>
-              <div className="info">
-                <div className="song-title">
+              <Flex column className="justify-around py-2">
+                <div className="relative text-[1em] text-[#3c3c3c]">
                   {activeTrack.title}
                   {false && (
-                    <div className="after-song-title">
+                    <Flex className="absolute left-full top-[2px] ml-2 w-full items-center text-[0.8em] text-[#7a7a7a]">
                       <div>Next: {nextTrack && nextTrack.title}&nbsp;</div>
-                      <i className="fas fa-angle-down" />
-                    </div>
+                      <i className="fa fa-angle-down cursor-pointer" />
+                    </Flex>
                   )}
                 </div>
 
@@ -237,15 +109,15 @@ class Player extends Component<PlayerProps, PlayerState> {
                   as={`/${artistSlug}/${year}/${month}/${day}?source=${source}`}
                   legacyBehavior
                 >
-                  <a className="band-title">
+                  <a className="justify-center text-[0.8em] text-[#7a7a7a]">
                     {bandTitle} – {removeLeadingZero(month)}/{removeLeadingZero(day)}/
                     {year.slice(2)}
                   </a>
                 </Link>
-              </div>
-              <div className="timing duration">
+              </Flex>
+              <div className="absolute left-[8px] right-[8px] top-1/2 translate-x-0 translate-y-[-50%] text-right text-[0.8em] text-[#7a7a7a]">
                 <div>
-                  <i className="fas fa-forward" onClick={() => player.playNext()} />
+                  <i className="fa fa-forward cursor-pointer" onClick={() => player.playNext()} />
                 </div>
                 <div onClick={this.toggleRemainingDuration}>
                   {durationToHHMMSS(
@@ -255,24 +127,27 @@ class Player extends Component<PlayerProps, PlayerState> {
                   )}
                 </div>
               </div>
-            </div>
+            </Flex>
             <div
-              className="progress-container"
+              className="absolute bottom-0 left-0 z-[1] h-1 w-full cursor-pointer bg-[#bcbcbc]"
               onClick={this.onProgressClick}
               style={{ opacity: playback.activeTrack.currentTime < 0.1 ? 0.8 : null }}
             >
               <div
-                className="progress-background"
+                className="absolute bottom-0 left-0 h-1 bg-[#707070]"
                 style={{ width: notchPosition ? notchPosition + 2 : null }}
               />
               <div
-                className="progress-notch"
+                className="absolute bottom-0 left-0 z-[1] h-2 w-[3px] bg-black"
                 style={{ transform: `translate(${notchPosition}px, 0)` }}
               />
             </div>
-            <div className="volume-container" onClick={this.setVolume}>
+            <div
+              className="absolute right-[-6px] top-0 h-full w-[6px] bg-[#0000001a]"
+              onClick={this.setVolume}
+            >
               <div
-                className="volume-bar"
+                className="pointer-events-none absolute bottom-0 left-0 right-0 top-0 bg-[#707070]"
                 style={{
                   top: `${(1 - this.state.volume) * 100}%`,
                 }}
@@ -286,12 +161,12 @@ class Player extends Component<PlayerProps, PlayerState> {
             as={`/${artistSlug}/${year}/${month}/${day}?source=${source}`}
             legacyBehavior
           >
-            <div className="queue-button">
-              <i className="fas fa-list-ol" />
+            <div className="w-[50px] cursor-pointer items-center justify-center self-center text-[#7a7a7a] active:text-[#4a4a4a]">
+              <i className="fa fa fa-list-ol cursor-pointer" />
             </div>
           </Link>
         )}
-      </div>
+      </Flex>
     );
   }
 
