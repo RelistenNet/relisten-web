69fb19e941ef6b5d48725cb4508d8b19c5ea5f0c
Daniel Saewitz <git@saewitz.com>
2025-04-01 01:53:42 -0400

    Fix type

--- src/lib/sortSources.ts
+++ src/lib/sortSources.ts
@@ -1,3 +1,4 @@
+import { Source } from '@/types';
 import { firstBy } from 'thenby';
 
 const getEtreeId = (s = '') =>
@@ -10,20 +11,21 @@ const getEtreeId = (s = '') =>
 
 // tapes: TODO: GD sort (charlie miller, sbd + etree id, weighted average), sbd + etree id, weighted avg, asc, desc
 // for now, hardcode sort: sbd, charlie miller, etree id, weighted average
-export const sortSources = (sources) => {
+export const sortSources = (sources: Source[]) => {
   const sortedSources = sources
     ? [...sources].sort(
-        firstBy((t) => t.is_soundboard, 'desc')
+        firstBy((t: Source) => t.is_soundboard, 'desc')
           // Charlie for GD, Pete for JRAD
           .thenBy(
-            (t) =>
+            (t: Source) =>
               /(charlie miller)|(peter costello)/i.test(
                 [t.taper, t.transferrer, t.source].join('')
               ),
             'desc'
           )
           .thenBy(
-            (t1, t2) => getEtreeId(t1.upstream_identifier) - getEtreeId(t2.upstream_identifier),
+            (t1: Source, t2: Source) =>
+              getEtreeId(t1.upstream_identifier) - getEtreeId(t2.upstream_identifier),
             'desc'
           )
           .thenBy((t) => t.avg_rating_weighted, 'desc')
