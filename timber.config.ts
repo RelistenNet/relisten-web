a400b09374d83a65716330b27b57d563a9d5ac42
Daniel Saewitz <git@saewitz.com>
2026-03-23 02:53:26 -0400

    upgrade @timber-js/app to alpha.14, enable detailed Server-Timing

--- timber.config.ts
+++ timber.config.ts
@@ -3,4 +3,5 @@ import { nitro } from '@timber-js/app/adapters/nitro';
 export default {
   output: 'server' as const,
   adapter: nitro({ preset: 'node-server' }),
+  serverTiming: 'detailed' as const,
 };
