07b09b9c8e192380b75d5940597914735dae6674
Daniel Saewitz <git@saewitz.com>
2026-06-15 14:05:39 -0400

    Improved Colors

--- src/app/(browse)/[artistSlug]/[year]/[month]/[day]/[songSlug]/params.ts
+++ src/app/(browse)/[artistSlug]/[year]/[month]/[day]/[songSlug]/params.ts
@@ -2,5 +2,5 @@ import { defineSearchParams } from '@timber-js/app/search-params';
 import { z } from 'zod/v4';
 
 export const searchParams = defineSearchParams({
-  sourceId: z.string(),
+  sourceId: z.string().optional(),
 });
